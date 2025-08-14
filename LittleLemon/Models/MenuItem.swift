a2b5d5d7d046a5da1ae6ce9c822debe22c7915fe
Quân Đinh <17819030+urjhams@users.noreply.github.com>
2025-08-14 18:29:21 +0200

    create model structs

--- LittleLemon/Models/MenuItem.swift
+++ LittleLemon/Models/MenuItem.swift
@@ -6,3 +6,9 @@
 //
 
 import Foundation
+
+struct MenuItem: Decodable {
+  let title: String
+  let image: String
+  let price: String
+}
--- LittleLemon/Models/MenuList.swift
+++ LittleLemon/Models/MenuList.swift
@@ -6,3 +6,7 @@
 //
 
 import Foundation
+
+struct MenuList: Decodable {
+  var menu: [MenuItem]
+}
