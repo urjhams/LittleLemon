72b040c45415a501331a6a890572dbf07bbaf563
Quân Đinh <17819030+urjhams@users.noreply.github.com>
2025-08-14 18:55:26 +0200

    add DataManager and tests

---
+++ LittleLemon/Utilities/DataManager.swift
@@ -0,0 +1,23 @@
+//
+//  DataManager.swift
+//  LittleLemon
+//
+//  Created by Quân Đinh on 14.08.25.
+//
+
+import Foundation
+
+public actor DataManager {
+  
+  var session: URLSession
+  
+  public init(_ session: URLSession = .shared) {
+    self.session = session
+  }
+  
+  func getMenuData() async throws -> MenuList {
+    let address = "https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/menu.json"
+    let (data, _) = try await session.data(from: URL(string: address)!)
+    return try JSONDecoder().decode(MenuList.self, from: data)
+  }
+}
